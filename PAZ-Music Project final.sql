8a67e7e4774de61b64c8f5f0d7ecbf79891aa6e1
PaziBack <sshipi@gmail.com>
2025-03-03 01:38:18 +0200

    Add files via upload
    Project 1 in SQL - create DB 
    Project 2 in SQL Advanced - Analyse
    Project 3 in Python -Analysing Music streaming - using Pandas, Matplotlib, numpy...

---
+++ PAZ-Music Project final.sql
@@ -0,0 +1,234 @@
+﻿Create DATABASE Records_pro
+use records_pro
+
+GO
+
+CREATE TABLE Genres (
+    GenreID INT IDENTITY(1,1) PRIMARY KEY, -- מזהה ייחודי לז'אנר
+    GenreName NVARCHAR(100) NOT NULL UNIQUE, -- שם הז'אנר
+    Description NVARCHAR(255), -- תיאור הז'אנר
+    Popularity INT CHECK (Popularity >= 0 AND Popularity <= 100)) -- פופולריות בין 0 ל-10
+ALTER TABLE Genres
+ADD CONSTRAINT CK_Popularity CHECK (Popularity >= 1 AND Popularity <= 10)
+
+
+CREATE TABLE Artists (
+    ArtistID INT IDENTITY(1,1) PRIMARY KEY, -- מזהה ייחודי לאמן
+    Name NVARCHAR(100) NOT NULL UNIQUE, -- שם האמן
+    DebutYear INT CHECK (DebutYear > 1900 AND DebutYear <= YEAR(GETDATE()))) -- שנת הבכורה
+ALTER TABLE Artists
+ADD GenreID INT NOT NULL
+ALTER TABLE Artists
+ADD CONSTRAINT FK_Artists_Genres FOREIGN KEY (GenreID) REFERENCES Genres(GenreID)
+
+CREATE TABLE Songs (
+    SongID INT IDENTITY(1,1) PRIMARY KEY, -- מזהה ייחודי לשיר
+    TrackName NVARCHAR(100) NOT NULL, -- שם השיר
+    ArtistID INT NOT NULL, -- מזהה האמן (FK ל-Artists)
+    GenreID INT NOT NULL, -- מזהה הז'אנר (FK ל-Genres)
+    ReleaseYear INT CHECK (ReleaseYear > 1900 AND ReleaseYear <= YEAR(GETDATE())), -- שנת יציאה
+    FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID),
+    FOREIGN KEY (GenreID) REFERENCES Genres(GenreID))
+
+CREATE TABLE Users (
+    UserID INT IDENTITY(1,1) PRIMARY KEY, -- מזהה ייחודי למשתמש
+    UserName NVARCHAR(100) NOT NULL UNIQUE, -- שם המשתמש
+    Age INT NOT NULL CHECK (Age > 0), -- גיל (חובה, מעל 0)
+    WhileWorking NVARCHAR(10), -- האם מאזינים בזמן עבודה
+    HrsPerDay FLOAT CHECK (HrsPerDay >= 0), -- שעות האזנה יומיות
+    FavGenre INT NOT NULL, -- מזהה הז'אנר המועדף (FK ל-Genres)
+    FOREIGN KEY (FavGenre) REFERENCES Genres(GenreID)
+)
+
+CREATE TABLE MusicEffect (
+    EffectID INT IDENTITY(1,1) PRIMARY KEY, -- מזהה ייחודי להשפעה
+    UserID INT NOT NULL, -- מזהה המשתמש (FK ל-Users)
+    GenreID INT NOT NULL, -- מזהה הז'אנר (FK ל-Genres)
+    MoodEffect NVARCHAR(255), -- תיאור השפעה על מצב הרוח
+    FrequencyClassical FLOAT CHECK (FrequencyClassical >= 0), -- תדירות האזנה לקלאסית
+    Anxiety NVARCHAR(10), -- חרדה (כן/לא)
+    Depression NVARCHAR(10), -- דיכאון (כן/לא)
+    Insomnia NVARCHAR(10), -- נדודי שינה (כן/לא)
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN KEY (GenreID) REFERENCES Genres(GenreID))
+
+
+INSERT INTO Genres (GenreName,Description, Popularity)
+VALUES ('Pop', 'Contemporary popular music', 10),
+		('Rock', 'Guitar-based music', 9),
+		('Jazz', 'Improvisational music', 7),
+		('Classical', 'Orchestral compositions', 6),
+		('Hip-Hop', 'Beat-driven vocal style', 10),
+		('Electronic', 'Synthesizer-based music', 6),
+		('R&B', 'Soulful-rhythmic and smooth melodies', 8),
+		('Reggae', 'Jamaican, offbeat rhythms, soulful', 7),
+		('Country', 'Storytelling, southern, acoustic-based', 8),
+		('Blues', 'Melancholic, expressive, guitar-driven', 7),
+		('Metal', 'Distorted guitars, aggressive vocals', 8),
+		('Punk Rock', 'Fast-paced, rebellious, raw', 7),
+		('Folk', 'Acoustic, traditional, narrative-driven', 6),
+		('Soul', 'Emotional, heartfelt vocal delivery', 7),
+		('House', 'Repetitive beats, dancefloor-oriented', 7),
+		('Techno', 'Electronic, repetitive, futuristic', 7),
+		('Latin', 'Latin American, rhythmic, danceable', 8),
+		('Alternative Rock', 'Experimental, diverse, non-mainstream', 8),
+		('Indie Rock', 'Independent, unique, alternative rock', 7),
+		('K-Pop', 'Pop, Korean, visually captivating', 9)
+
+
+INSERT INTO Artists (Name, DebutYear, GenreID)
+VALUES
+	--('Queen', 1970),
+	--('Ludwig van Beethoven', 1800)
+    ('Jack Jackson', 2010, 3),  -- Pop
+    ('The Weeknd', 2010, 3),    -- Pop
+    ('Dua Lipa', 2015, 3),      -- Pop
+    ('The Kid LAROI & Justin Bieber', 2020, 3), -- Pop
+    ('Ed Sheeran', 2011, 3),    -- Pop
+    ('Lewis Capaldi', 2018, 3), -- Pop
+    ('Lil Nas X', 2019, 7),     -- Country
+    ('Billie Eilish', 2015, 3), -- Pop
+    ('Olivia Rodrigo', 2020, 3), -- Pop
+    ('Bob Dylan', 1961, 4),     -- Folk
+    ('Uncle Tupelo', 1987, 7),  -- Country
+    ('Kendrick Lamar', 2011, 5), -- Hip-Hop
+    ('Taylor Swift', 2006, 3),  -- Pop
+    ('Mark Ronson ft. Bruno Mars', 2014, 3), -- Pop
+    ('Queen', 1970, 6),         -- Rock
+    ('Eagles', 1971, 6),        -- Rock
+    ('Nirvana', 1987, 6),       -- Rock
+    ('Dave Brubeck', 1940, 8),  -- Jazz
+    ('Miles Davis', 1944, 8),   -- Jazz
+    ('John Coltrane', 1955, 8), -- Jazz
+    ('Travis Scott', 2012, 5),  -- Hip-Hop
+    ('Deadmau5', 2005, 9),      -- Electronic
+    ('David Guetta ft. Sia', 2002, 9), -- Electronic
+    ('Martin Garrix', 2013, 9), -- Electronic
+    ('Harry Styles', 2010, 2),  -- R&B
+    ('Blackstreet', 1991, 2),   -- R&B
+    ('SZA', 2013, 2),           -- R&B
+    ('Bob Marley & The Wailers', 1962, 10), -- Reggae
+    ('Bob Marley', 1962, 10),   -- Reggae
+    ('Inner Circle', 1968, 10), -- Reggae
+    ('John Denver', 1964, 7),   -- Country
+    ('Dolly Parton', 1967, 7),  -- Country
+    ('Carrie Underwood', 2005, 7), -- Country
+    ('BTS', 2013, 11),          -- K-Pop
+    ('PSY', 2000, 11),          -- K-Pop
+    ('BLACKPINK', 2016, 11),    -- K-Pop
+    ('Rick James', 1978, 12),   -- Funk
+    ('Luis Fonsi ft. Daddy Yankee', 2000, 13), -- Latin
+    ('Ritchie Valens', 1958, 13), -- Latin
+    ('Ricky Martin', 1991, 13); -- Latin
+
+
+INSERT INTO Songs (TrackName, ArtistID, GenreID, ReleaseYear)
+VALUES
+ ('Anti-Hero', 13, 1, 2022), -- Taylor Swift, Pop
+    ('As It Was', 16, 1, 2022), -- Harry Styles, Pop
+    ('Unholy', 17, 1, 2022), -- Sam Smith ft. Kim Petras, Pop
+    ('Flowers', 18, 1, 2023), -- Miley Cyrus, Pop
+    ('Creepin', 2, 2, 2022), -- The Weeknd ft. Metro Boomin, Hip-Hop
+    ('Calm Down', 19, 1, 2023), -- Rema ft. Selena Gomez, Pop
+    ('Kill Bill', 20, 10, 2023), -- SZA, R&B
+    ('Golden Hour', 21, 1, 2023), -- JVKE, Pop
+    ('Escapism.', 22, 10, 2022), -- RAYE ft. 070 Shake, R&B
+    ('I’m Good (Blue)', 23, 8, 2022); -- David Guetta ft. Bebe Rexha, Electronic
+--('Bohemian Rhapsody', 1, 1, 1975),
+--('Symphony No. 5', 2, 2, 1808),
+INSERT INTO Songs (TrackName, ArtistID, GenreID, ReleaseYear)
+VALUES
+       ('Banana Pancakes', 1, 1, 2010), -- Jack Jackson, Pop
+       ('Blinding Lights', 2, 1, 2020), -- The Weeknd, Pop
+       ('Levitating', 3, 1, 2020), -- Dua Lipa, Pop
+       ('Save Your Tears', 2, 1, 2020), -- The Weeknd, Pop
+       ('Stay', 4, 1, 2021), -- The Kid LAROI & Justin Bieber, Pop
+       ('Shape of You', 5, 1, 2017), -- Ed Sheeran, Pop
+       ('Someone You Loved', 6, 1, 2018), -- Lewis Capaldi, Pop
+       ('Old Town Road', 7, 3, 2019), -- Lil Nas X, Country
+       ('Bad Guy', 8, 1, 2019), -- Billie Eilish, Pop
+       ('Drivers License', 9, 1, 2021), -- Olivia Rodrigo, Pop
+       ('The Times They Are a Changin', 10, 4, 1964), -- Bob Dylan, Folk
+       ('Moonshine', 11, 3, 1993), -- Uncle Tupelo, Country
+       ('HUMBLE.', 12, 2, 2017), -- Kendrick Lamar, Hip-Hop
+       ('Shake It Off', 13, 1, 2014), -- Taylor Swift, Pop
+       ('Bohemian Rhapsody', 14, 6, 1975), -- Queen, Rock
+       ('Hotel California', 15, 6, 1976), -- Eagles, Rock
+       ('Take Five', 18, 7, 1959), -- Dave Brubeck, Jazz
+       ('Titanium', 22, 8, 2011), -- David Guetta ft. Sia, Electronic
+       ('Strobe', 21, 8, 2009), -- Deadmau5, Electronic
+       ('No Woman, No Cry', 25, 9, 1974); -- Bob Marley & The Wailers, Reggae
+
+
+
+INSERT INTO Users (UserName, Age, WhileWorking, HrsPerDay, FavGenre)
+VALUES
+    ('Alice', 25, 'Yes', 2, 1),   -- כאן 1 מתאר את ה-GenreID של "Pop"
+    ('Bob', 34, 'No', 1, 2),   -- 2 מתאר את ה-GenreID של "Rock"
+    ('Charlie', 29, 'No', NULL, 3),   -- 3 מתאר את ה-GenreID של "Jazz"
+    ('Diana', 42, 'Yes', 2, 4),   -- 4 מתאר את ה-GenreID של "Classical"
+    ('Eve', 19, 'No', 1, 5),   -- 5 מתאר את ה-GenreID של "Hip-Hop"
+    ('Frank', 31, 'Yes', 3, 6),   -- 6 מתאר את ה-GenreID של "Electronic"
+    ('Grace', 28, 'No', 2, 7),   -- 7 מתאר את ה-GenreID של "Reggae"
+    ('Hank', 36, 'Yes', 3, 8),   -- 8 מתאר את ה-GenreID של "Blues"
+    ('Ivy', 22, 'No', 1, 9),   -- 9 מתאר את ה-GenreID של "Pop"
+    ('Jack', 40, 'Yes', 2, 10),   -- 10 מתאר את ה-GenreID של "Jazz"
+    ('Zoe Mitchell', 21, 'No', 1, 11),   -- 11 מתאר את ה-GenreID של "Classical"
+    ('Ethan Baker', 37, 'Yes', 2, 12),   -- 12 מתאר את ה-GenreID של "Reggae"
+    ('Lila Gear', 31, 'Yes', 3, 13),   -- 13 מתאר את ה-GenreID של "Rock"
+    ('Oscar Martinez', 28, 'No', 1, 14),   -- 14 מתאר את ה-GenreID של "Hip-Hop"
+    ('Sofia Hernandez', 24, 'Yes', 2, 15),   -- 15 מתאר את ה-GenreID של "Pop"
+    ('Leo Fisher', 33, 'Yes', 3, 16),   -- 16 מתאר את ה-GenreID של "Classical"
+    ('Ava Patel', 26, 'No', 1, 17),   -- 17 מתאר את ה-GenreID של "Jazz"
+    ('Mason Nguyen', 40, 'Yes', 3, 18),   -- 18 מתאר את ה-GenreID של "Blues"
+    ('Chloe Kim', 30, 'No', 2, 19),   -- 19 מתאר את ה-GenreID של "Pop"
+    ('Lucas Wang', 35, 'Yes', 1, 20);   -- 20 מתאר את ה-GenreID של "Electronic"
+
+
+SELECT*
+FROM Users
+
+-- יוזר מתחיל ב41
+INSERT INTO MusicEffect (UserID, GenreID, MoodEffect, FrequencyClassical, Anxiety, Depression, Insomnia)
+VALUES
+    (41, 1, 'Helps to focus', 3.0, 'No', 'No', 'No'),
+    (42, 2, 'Reduces stress', 4.0, 'Yes', 'No', 'No'),
+    (43, 3, 'Improves mood', 2.0, 'No', 'Yes', 'Yes');
+    -- כל שאר הנתונים של MusicEffect
+
+INSERT INTO MusicEffect (UserID, GenreID, MoodEffect, FrequencyClassical, Anxiety, Depression, Insomnia)
+VALUES
+    (44, 4, 'Relaxing', 5.0, 'Yes', 'No', 'No'),          -- Diana, Classical
+    (45, 5, 'Boosts energy', 3.0, 'No', 'Yes', 'No'),     -- Eve, Hip-Hop
+    (46, 6, 'Calms nerves', 4.5, 'Yes', 'No', 'Yes'),     -- Frank, Electronic
+    (47, 7, 'Lifts mood', 2.5, 'No', 'Yes', 'No'),        -- Grace, Reggae
+    (48, 8, 'Enhances creativity', 3.5, 'Yes', 'No', 'Yes'), -- Hank, Blues
+    (49, 9, 'Improves focus', 2.0, 'No', 'No', 'Yes'),    -- Ivy, Pop
+    (50, 10, 'Boosts concentration', 4.0, 'Yes', 'No', 'No'), -- Jack, Jazz
+    (51, 11, 'Calms nerves', 3.0, 'Yes', 'Yes', 'No'),   -- Zoe Mitchell, Classical
+    (52, 12, 'Reduces anxiety', 4.0, 'No', 'No', 'Yes'), -- Ethan Baker, Reggae
+    (53, 13, 'Improves mood', 1.5, 'Yes', 'No', 'Yes'),  -- Lila Gear, Rock
+    (54, 14, 'Relieves stress', 2.5, 'Yes', 'No', 'No'), -- Oscar Martinez, Hip-Hop
+    (55, 15, 'Boosts focus', 3.0, 'No', 'Yes', 'Yes'),   -- Sofia Hernandez, Pop
+    (56, 16, 'Soothing', 5.0, 'Yes', 'No', 'No'),        -- Leo Fisher, Classical
+    (57, 17, 'Helps to focus', 4.0, 'Yes', 'Yes', 'No'), -- Ava Patel, Jazz
+    (58, 18, 'Improves mood', 3.0, 'No', 'No', 'Yes'),   -- Mason Nguyen, Blues
+    (59, 19, 'Enhances concentration', 2.0, 'Yes', 'No', 'No'), -- Chloe Kim, Pop
+    (60, 20, 'Reduces anxiety', 4.5, 'No', 'Yes', 'No'); -- Lucas Wang, Electronic
+
+
+	SELECT*
+	FROM MusicEffect
+
+	SELECT*
+	FROM users
+	ORDER BY FavGenre
+	SELECT*
+	FROM Songs
+	SELECT *
+	FROM Artists
+	SELECT*
+	FROM Genres
+
+
+
---
+++ Project 2 16-01-25 Paz.sql
@@ -0,0 +1,306 @@
+﻿
+--done V 1----------------------------------------------------------------------------- done V 1-------------
+WITH YearlyData
+AS (
+	SELECT YEAR(O.OrderDate) AS 'Year', 
+       SUM(OL.UnitPrice * OL.Quantity) AS 'IncomePerYear',
+       COUNT(DISTINCT MONTH(O.OrderDate)) AS 'NumOfDistinctMonths'
+	FROM Sales.Orders O
+	INNER JOIN Sales.OrderLines OL ON O.OrderID = OL.OrderID
+	JOIN Sales.Invoices I ON I.OrderID = O.OrderID
+	GROUP BY YEAR(O.OrderDate)
+),
+YearlyLinearIncome AS (
+    SELECT
+        Year,
+        IncomePerYear,
+        NumOfDistinctMonths,
+        (IncomePerYear / NumOfDistinctMonths) * 12 AS YearlyLinearIncome -- חישוב ההכנסה הלינארית
+    FROM YearlyData
+),
+YearlyGrowth AS (
+    SELECT
+        Year,
+        IncomePerYear,
+        NumOfDistinctMonths,
+        YearlyLinearIncome,
+        LAG(YearlyLinearIncome) OVER (ORDER BY Year) AS PreviousYearLinearIncome -- הכנסה לינארית לשנה הקודמת
+    FROM YearlyLinearIncome
+)
+SELECT
+    Year,
+    FORMAT(IncomePerYear, 'N0') AS IncomePerYear, 
+    NumOfDistinctMonths,
+    FORMAT(YearlyLinearIncome, 'N0') AS YearlyLinearIncome,
+CASE 
+    WHEN PreviousYearLinearIncome IS NULL THEN NULL
+	ELSE ( FORMAT(((YearlyLinearIncome - PreviousYearLinearIncome) * 100.0) / PreviousYearLinearIncome,'N2')) 
+    END AS GrowthRate --  שיעור הצמיחה
+FROM YearlyGrowth
+ORDER BY Year
+
+--done V 2--------------------------------------------------------------------------------------
+WITH RankedCustomers AS (
+    SELECT
+        YEAR(O.OrderDate) AS SalesYear,  -- שנה
+        DATEPART(QUARTER, O.OrderDate) AS SalesQuarter,  -- רבעון
+        C.CustomerName,  -- שם הלקוח
+        SUM(IL.UnitPrice * IL.Quantity) AS IncomePerYear,  -- הכנסה נטו
+        DENSE_RANK() OVER (PARTITION BY YEAR(O.OrderDate), DATEPART(QUARTER, O.OrderDate) ORDER BY SUM(IL.UnitPrice * IL.Quantity) DESC) AS RankOrder  -- דירוג
+    FROM Sales.Orders O
+    INNER JOIN Sales.Customers C ON O.CustomerID = C.CustomerID
+	INNER JOIN Sales.invoices inv ON O.orderID= inv.orderID
+	INNER JOIN sales.invoicelines IL ON inv.invoiceID= IL.invoiceID
+
+    GROUP BY YEAR(O.OrderDate), DATEPART(QUARTER, O.OrderDate), C.CustomerName
+)
+SELECT
+    SalesYear,
+    SalesQuarter,
+    CustomerName,
+    IncomePerYear,
+    RankOrder AS DNR  
+FROM RankedCustomers
+WHERE RankOrder <= 5  -- חמשת הלקוחות המובילים בכל רבעון
+ORDER BY SalesYear, SalesQuarter, RankOrder
+
+
+---done V3--3--------------------------------------------------------------------------
+-- עשרת המוצרים שהניבו את הרווח הכולל הגבוה ביותר על בסיס השורות שנמכרו
+--דרג את המוצרים לפי הרווח הכולל ושליפה של מזהה הפריט, שם הפריט,
+--והרווח הכולל שלהם
+
+SELECT TOP 10
+	SI.StockItemID, 
+	SI.StockItemName, 
+	SUM(IL.ExtendedPrice - IL.TaxAmount) AS TotalProfit  
+FROM Sales.InvoiceLines IL
+JOIN 
+			Warehouse.StockItems SI ON IL.StockItemID = SI.StockItemID
+GROUP BY 
+	SI.StockItemID, 
+	SI.StockItemName
+ORDER BY 
+	TotalProfit DESC
+
+
+--done v4---------4---------------------------------------------------------------------------
+--פריטי המלאי שתוקפם עדיין בתוקף,תחשב את הרווח
+--הנומינלי של כל פריט )ההפרש בין המחיר המומלץ לצרכן למחיר ליחידה(, ותדרג את הפריטים לפי
+--הרווח הנומינלי בסדר יורד. הציגו גם את המספר הסידורי של כל פריט בסדר זה.
+
+WITH NominalProfits
+AS	(SELECT 
+			StockItemID, StockItemName,unitprice,RecommendedRetailPrice
+			,(RecommendedRetailPrice-unitprice) AS NominalProductProfit  -- רווח נומינלי
+	 FROM warehouse.StockItems 
+	 WHERE GETDATE() BETWEEN ValidFrom AND ValidTo -
+)
+
+SELECT 
+ROW_NUMBER() OVER (ORDER BY RecommendedRetailPrice DESC) AS RN 
+,StockItemID, StockItemName,unitprice,RecommendedRetailPrice,NominalProductProfit,
+DENSE_RANK() OVER (order by NominalProductProfit DESC) AS DNR -- דירוג לפי רווח נומינלי בסדר יורד
+
+FROM
+	NominalProfits
+Order BY  RN
+---------------------------------------------------------------------------
+
+--שאילתה המציגה עבור כל קוד ספק ושם ספק את רשימת המוצרים במלאי עבור אותו
+--ספק, כשהרשימה מופרדת באמצעות ' , / '. יש לכלול בפרטי המוצר את קוד המוצר ושם המוצר ממלאי
+--המוצרים.
+--5 done v5 ----------------------------------------------------------------------------
+SELECT 
+CONCAT (S.SupplierID,' - ', S.SupplierName) AS SupplierDetails,
+STRING_AGG(CONCAT(SI.StockItemID,' ', SI.StockItemName), ' , / ') AS ProductDetails
+FROM 
+    purchasing.Suppliers S
+JOIN 
+    warehouse.StockItems SI ON S.SupplierID = SI.SupplierID 
+GROUP BY 
+		S.SupplierID, S.SupplierName
+ORDER BY
+		S.SupplierID
+
+-----------------------------------------------------------------------------
+--done v6------6---------------------------------------------------------------------
+
+SELECT TOP 5
+c.CustomerID
+,city.CityName
+,country.CountryName 
+,country.Continent
+,country.region
+,FORMAT(SUM(IL.ExtendedPrice),'N2') AS TotalExtendedPrice
+
+
+FROM Sales.Customers C
+
+JOIN application.cities city ON c.deliverycityID = city.cityID --חיבור עיר 
+JOIN application.StateProvinces SP ON city.StateProvinceID = SP.StateProvinceID -- חיבור למדינה  
+JOIN application.countries Country ON SP.countryID = Country.countryID -- חיבור מדינה ויבשת
+JOIN Sales.Orders orders ON C.CustomerID = orders.CustomerID -- חיבור הזמנות לפי מס לקוח
+JOIN Sales.Invoices invoice ON  Orders.OrderID= invoice.OrderID -- חיבור חשבוניות להזמנות
+JOIN Sales.InvoiceLines IL ON invoice.invoiceID = IL.InvoiceID -- חישוב הוצאה טוטאלי
+
+GROUP BY 
+        C.CustomerID, City.CityName, Country.CountryName, Country.Continent, Country.Region
+
+ORDER BY 
+		SUM(IL.ExtendedPrice) DESC
+
+
+--7 DoneV----------------------------------------------------------------------------------------------------------------------
+WITH MonthlyData AS (
+SELECT 
+        YEAR(O.OrderDate) AS OrderYear,
+        MONTH(O.OrderDate) AS OrderMonth,
+        SUM(OL.pickedQuantity * OL.UnitPrice) AS MonthlyTotal
+FROM 
+        Sales.Orders AS O
+JOIN 
+        Sales.OrderLines AS OL ON O.OrderID = OL.OrderID
+GROUP BY 
+        YEAR(O.OrderDate), MONTH(O.OrderDate)
+),
+CumulativeData AS (
+SELECT 
+        OrderYear,
+        OrderMonth,
+        MonthlyTotal,
+        SUM(MonthlyTotal) OVER (PARTITION BY OrderYear ORDER BY OrderMonth) AS CumulativeTotal
+FROM 
+        MonthlyData
+),
+
+FinalData AS (
+SELECT 
+        OrderYear,
+        OrderMonth,
+        MonthlyTotal,
+        CumulativeTotal
+
+FROM  CumulativeData
+
+UNION ALL
+
+SELECT 
+        OrderYear,
+        13 AS OrderMonth, -- שורת סיכום תופיע לאחר 12 החודשים
+         SUM(MonthlyTotal) AS MonthlyTotal,
+         SUM(MonthlyTotal) AS CumulativeTotal
+FROM 
+        MonthlyData
+GROUP BY 
+        OrderYear
+)
+
+SELECT 
+    OrderYear,
+CASE 
+    WHEN OrderMonth = 13 THEN 'GrandTotal'
+    ELSE CAST(OrderMonth AS VARCHAR)
+    END AS OrderMonth,
+    FORMAT(MonthlyTotal,'N0') AS MonthlyTotal ,
+    FORMAT(CumulativeTotal,'N0') AS CumulativeTotal
+FROM 
+    FinalData
+ORDER BY 
+    OrderYear
+
+--הציגו באמצעות מטריצה את מספר ההזמנות שנעשו בכל חודש בשנה
+--8--------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+WITH OrdersPerMonth AS (
+    SELECT
+        YEAR(OrderDate) AS OrderYear,     
+        MONTH(OrderDate) AS OrderMonth,  
+        COUNT(OrderID) AS OrderCount     
+    FROM 
+        Sales.Orders
+    GROUP BY 
+        YEAR(OrderDate), MONTH(OrderDate)
+)
+SELECT 
+    OrderMonth,                          
+    ISNULL([2013], 0) AS [2013],      
+    ISNULL([2014], 0) AS [2014],      
+    ISNULL([2015], 0) AS [2015],      
+    ISNULL([2016], 0) AS [2016]       
+FROM 
+    OrdersPerMonth
+PIVOT (
+    SUM(OrderCount)                     
+    FOR OrderYear IN ([2013], [2014], [2015], [2016]) 
+) AS PivotTable
+ORDER BY 
+    OrderMonth  
+
+------------------------------------------------------------------------------------
+--זהו לקוחות פוטנציאליים לנטישה על סמך דפוסי ההזמנות שלהם. לקוח נחשב כ"פוטנציאל
+--לנטישה" אם הזמן שחלף מאז ההזמנה האחרונה שלו גדול מפי שניים מהזמן הממוצע בין ההזמנות
+--שלו. הציגו עבור כל לקוח את מזהה הלקוח, שם הלקוח, תאריך ההזמנה האחרונה, מספר הימים
+--שחלפו מאז ההזמנה האחרונה, הזמן הממוצע בין ההזמנות )בימים(, וסטטוס הלקוח )"פוטנציאל
+--לנטישה" או "פעיל"(.
+--9-done v-----------------------------------------------------------------------------
+WITH T AS (
+    SELECT 
+        C.CustomerID,
+        C.CustomerName,
+        O.OrderDate,
+        MAX(O.OrderDate) OVER (PARTITION BY C.CustomerID) AS LastOrder,
+        LAG(O.OrderDate, 1) OVER (PARTITION BY C.CustomerID ORDER BY O.OrderDate) AS PreviousOrderDate,
+        DATEDIFF(DAY, LAG(O.OrderDate, 1) OVER (PARTITION BY C.CustomerID ORDER BY O.OrderDate), O.OrderDate) AS DaysSinceLastOrder1
+    FROM Sales.Orders O
+    JOIN Sales.Customers C
+        ON O.CustomerID = C.CustomerID
+)
+SELECT 
+    T.CustomerID,
+    T.CustomerName,
+    T.OrderDate,
+    T.PreviousOrderDate,
+    DATEDIFF(DAY, LastOrder, '2016-05-31') AS DaysSinceLastOrder,
+    AVG(DATEDIFF(DAY, PreviousOrderDate, OrderDate)) OVER (PARTITION BY CustomerID) AS AvgDaysBetweenOrders,
+    IIF(
+        AVG(DATEDIFF(DAY, PreviousOrderDate, OrderDate)) OVER (PARTITION BY CustomerID) * 2 > DATEDIFF(DAY, LastOrder, '2016-05-31'),
+        'Active',
+        'Potential churn'
+    ) AS CustomerStatus
+FROM T
+
+--10--done v-------------------------------------------------------
+WITH CategorizedCustomers AS (
+    
+	SELECT
+	CASE
+            WHEN CustomerName LIKE 'Wingtip%' THEN 'Wingtip Customers'
+            WHEN CustomerName LIKE 'Tailspin%' THEN 'Tailspin Customers'
+            ELSE CustomerName
+		    END AS GeneralizedCustomerName,
+            cc.CustomerCategoryName AS CustomerCategoryName
+	FROM Sales.Customers c
+	JOIN Sales.CustomerCategories cc
+    ON c.CustomerCategoryID = cc.CustomerCategoryID
+),
+CategoryCustomerCount AS (
+    SELECT
+        CustomerCategoryName,
+        COUNT(DISTINCT GeneralizedCustomerName) AS CustomerCount
+    FROM CategorizedCustomers
+    GROUP BY CustomerCategoryName
+),
+TotalCustomers AS (
+    SELECT
+        SUM(CustomerCOUNT) AS TotalCustCount
+    FROM CategoryCustomerCount
+)
+SELECT
+    c.CustomerCategoryName,
+    c.CustomerCount,
+    t.TotalCustCount,
+    CONCAT(ROUND(CAST(c.CustomerCount AS FLOAT) / t.TotalCustCount * 100, 2),'%') AS DistributionFactor
+FROM CategoryCustomerCount c
+CROSS JOIN TotalCustomers t
+ORDER BY  CustomerCategoryName
